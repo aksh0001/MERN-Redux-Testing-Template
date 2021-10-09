cfce4096b83ab952ff7bcceeadb8a15ac7ad61e7
akshay <akshay.k1738@gmail.com>
2021-10-08 21:37:16 -0500

    Final

---
+++ client/src/components/App.jsx
@@ -0,0 +1,22 @@
+/**
+ * Root component.
+ */
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Home from './home/Home';
+
+const App = () => {
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
